f56d587d622f523eedc1c8d0b46f23b387ac3705
PawelPerek <pawelperek99@gmail.com>
2023-08-19 19:32:57 +0200

    Implement vdiv/vrem

--- src/rv_core/instruction/executor/v/vdiv.rs
+++ src/rv_core/instruction/executor/v/vdiv.rs
@@ -5,10 +5,40 @@ use crate::rv_core::{
     registers::{VectorRegisters, IntegerRegisters}
 };
 
-pub fn vv(Opmvv { dest, vs1, vs2, vm }: Opmvv, v: &mut VectorRegisters) {
-    todo!()
+pub fn vv(Opmvv { dest: vd, vs1, vs2, vm }: Opmvv, v: &mut VectorRegisters) {
+    let vreg = izip!(
+        v.get(vs2).iter_eew(),
+        v.get(vs1).iter_eew()
+    ).masked_map(
+        v.default_mask(vm),
+        v.get(vd).iter_eew(),
+        |(dividend, divisor)| {
+            if divisor == 0 { 
+                u64::MAX
+            } else { 
+                ((dividend as i64) / (divisor as i64)) as u64
+            }
+        }
+    ).collect_with_eew(v.vec_engine.sew.clone());
+
+    v.apply(vd, vreg);
 }
 
-pub fn vx(Opmvx { dest, rs1, vs2, vm }: Opmvx, v: &mut VectorRegisters, x: &IntegerRegisters) {
-    todo!()
+pub fn vx(Opmvx { dest: vd, rs1, vs2, vm }: Opmvx, v: &mut VectorRegisters, x: &IntegerRegisters) {
+    let vreg = v.get(vs2).iter_eew()
+    .masked_map(
+        v.default_mask(vm),
+        v.get(vd).iter_eew(),
+        |dividend| {
+            let divisor = x[rs1];
+
+            if divisor == 0 { 
+                u64::MAX
+            } else { 
+                ((dividend as i64) / (divisor as i64)) as u64
+            }
+        }
+    ).collect_with_eew(v.vec_engine.sew.clone());
+
+    v.apply(vd, vreg);
 }
