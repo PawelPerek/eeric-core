153d07109376bb742e06d2eca949e55dc0baf3f4
Paweł Perek <pawel@perek.pro>
2023-08-20 12:47:48 +0200

    Fix tests

--- src/rv_core/instruction/executor/base/sll.rs
+++ src/rv_core/instruction/executor/base/sll.rs
@@ -6,5 +6,5 @@ use crate::rv_core::{
 pub fn sll(R { rd, rs1, rs2 }: R, x: &mut IntegerRegisters) {
     let rs1 = x[rs1];
     let shamt5 = x[rs2] & 0b11111;
-    x[rd] = rs1 << rs2;
+    x[rd] = rs1 << shamt5;
 }
