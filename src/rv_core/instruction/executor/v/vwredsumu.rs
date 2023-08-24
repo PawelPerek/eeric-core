71b2e011319a87801c56d893ba566f7dc30ea41b
PawelPerek <pawelperek99@gmail.com>
2023-08-24 15:30:30 +0200

    Implement widening reduction operations

--- src/rv_core/instruction/executor/v/vwredsumu.rs
+++ src/rv_core/instruction/executor/v/vwredsumu.rs
@@ -3,5 +3,16 @@ use crate::prelude::*;
 use crate::rv_core::{instruction::format::Opivv, registers::VectorRegisters};
 
 pub fn vs(Opivv { vd, vs1, vs2, vm }: Opivv, v: &mut VectorRegisters) {
-    todo!()
+    let initial_value = v.get_wide(vs1).iter_eew().next().unwrap();
+    let sum = izip!(v.get(vs2).iter_eew(), v.default_mask(vm))
+        .fold(initial_value, |acc, (vs2, mask)| acc.wrapping_add(vs2 as u128 * mask as u128));
+
+    let mut vd_data = v.get_wide(vd).iter_eew().collect_vec();
+    vd_data[0] = sum;
+
+    let vreg = vd_data
+        .into_iter()
+        .collect_with_wide_eew(v.vec_engine.sew.clone());
+
+    v.apply(vd, vreg);
 }
