6b5d794d8323132557b8c1dc63a151ff6e2b0673
PawelPerek <pawelperek99@gmail.com>
2023-08-25 13:57:56 +0200

    Add 32-bit fma instructions

--- src/rv_core/instruction/executor/f/fnmadd.rs
+++ src/rv_core/instruction/executor/f/fnmadd.rs
@@ -3,5 +3,10 @@ use crate::rv_core::{instruction::format::R4, registers::FloatRegisters};
 use super::utils::convert::{compose, decompose};
 
 pub fn s(R4 { rd, rs1, rs2, rs3 }: R4, f: &FloatRegisters) {
-    todo!()
+    let (fs1, _) = decompose(f[rs1]);
+    let (fs2, _) = decompose(f[rs2]);
+    let (fs3, _) = decompose(f[rs3]);
+    let (_, rest) = decompose(f[rd]);
+
+    f[rd] = compose(-fs1 * fs2 - fs3, rest);
 }
