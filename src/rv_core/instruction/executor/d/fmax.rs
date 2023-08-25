ebcf90e3a810a127490dc723bf4044a91f375a22
PawelPerek <pawelperek99@gmail.com>
2023-08-25 16:00:11 +0200

    Implement f64 max/min/mul/div

--- src/rv_core/instruction/executor/d/fmax.rs
+++ src/rv_core/instruction/executor/d/fmax.rs
@@ -1,5 +1,5 @@
 use crate::rv_core::{instruction::format::R, registers::FloatRegisters};
 
-pub fn d(R { rd, rs1, rs2 }: R, f: &FloatRegisters) {
-    todo!()
+pub fn d(R { rd, rs1, rs2 }: R, f: &mut FloatRegisters) {
+    f[rd] = if f[rs1] > f[rs2] { f[rs1] } else { f[rs2] };
 }
