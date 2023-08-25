63608048aaff06456b5211e9806df5593abe87a8
PawelPerek <pawelperek99@gmail.com>
2023-08-25 16:10:38 +0200

    Implement f64 fsgnj instructions

--- src/rv_core/instruction/executor/d/fsgnjn.rs
+++ src/rv_core/instruction/executor/d/fsgnjn.rs
@@ -1,5 +1,6 @@
 use crate::rv_core::{instruction::format::R, registers::FloatRegisters};
 
-pub fn d(R { rd, rs1, rs2 }: R, f: &FloatRegisters) {
-    todo!()
+pub fn d(R { rd, rs1, rs2 }: R, f: &mut FloatRegisters) {
+    f[rd] = f[rs1].copysign(-f[rs2]);
+
 }
--- src/rv_core/instruction/executor/d/fsgnjx.rs
+++ src/rv_core/instruction/executor/d/fsgnjx.rs
@@ -1,5 +1,12 @@
 use crate::rv_core::{instruction::format::R, registers::FloatRegisters};
 
-pub fn d(R { rd, rs1, rs2 }: R, f: &FloatRegisters) {
-    todo!()
+pub fn d(R { rd, rs1, rs2 }: R, f: &mut FloatRegisters) {
+    let (fs1_sign, fs2_sign) = (
+        if f[rs1].is_sign_positive() { 0 } else { 1 },
+        if f[rs2].is_sign_positive() { 0 } else { 1 }
+    );
+
+    let result_sign = fs1_sign ^ fs2_sign;
+
+    f[rd] = if result_sign == 0 { f[rs1] } else { -f[rs1] };
 }
