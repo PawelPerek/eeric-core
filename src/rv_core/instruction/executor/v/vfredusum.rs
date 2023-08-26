5e69498feb8d9d244e19a7fd2acb65d1e82ae113
PawelPerek <pawelperek99@gmail.com>
2023-08-26 21:21:22 +0200

    Implement vf reduction operations

--- src/rv_core/instruction/executor/v/vfredusum.rs
+++ src/rv_core/instruction/executor/v/vfredusum.rs
@@ -2,6 +2,28 @@ use crate::prelude::*;
 
 use crate::rv_core::{instruction::format::Opfvv, registers::VectorRegisters};
 
-pub fn vs(Opfvv { dest, vs1, vs2, vm }: Opfvv, v: &mut VectorRegisters) {
-    todo!()
+// TODO: binary tree addition implementation separate from vfredosum to show differences. 
+// But since vfredosum is a valid implementation, it can be used for now.
+
+pub fn vs(Opfvv { dest: vd, vs1, vs2, vm }: Opfvv, v: &mut VectorRegisters) {
+    let initial_value = v.get(vs1).iter_fp().next().unwrap();
+    let sum = izip!(v.get(vs2).iter_fp(), v.default_mask(vm)).fold(
+        initial_value,
+        |acc, (vs2, mask)| {
+            if mask == 1 {
+                acc + vs2
+            } else {
+                acc
+            }
+        },
+    );
+
+    let mut vd_snapshot = v.get(vd).iter_fp().collect_vec();
+    vd_snapshot[0] = sum;
+
+    let vreg = vd_snapshot
+        .into_iter()
+        .collect_fp();
+
+    v.apply(vd, vreg);
 }
