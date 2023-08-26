d28db14e479b70d9683b2a9b66ae8e1cc797d0f0
PawelPerek <pawelperek99@gmail.com>
2023-08-26 21:55:11 +0200

    Implement vfslide1up/down

--- src/rv_core/instruction/executor/v/vfslide1down.rs
+++ src/rv_core/instruction/executor/v/vfslide1down.rs
@@ -1,10 +1,25 @@
 use crate::prelude::*;
 
+use crate::rv_core::registers::vector::Vreg;
 use crate::rv_core::{
     instruction::format::Opfvf,
     registers::{FloatRegisters, VectorRegisters},
 };
 
 pub fn vf(Opfvf { vd, rs1, vs2, vm }: Opfvf, v: &mut VectorRegisters, f: &FloatRegisters) {
-    todo!()
+    let last_value = f64::to_le_bytes(f[rs1]);
+
+    let vreg_values: Vreg = v
+        .get(vs2)
+        .iter_byte()
+        .take(v.vec_engine.vlmax() - v.vec_engine.sew.byte_length())
+        .chain(last_value[0..v.vec_engine.sew.byte_length()].into_iter().copied())
+        .collect();
+
+    let vreg = vreg_values
+        .iter_eew()
+        .masked_map(v.default_mask(vm), v.get(vd).iter_eew(), |vd| vd)
+        .collect_with_eew(v.vec_engine.sew.clone());
+
+    v.apply(vd, vreg);
 }
--- src/rv_core/instruction/executor/v/vfslide1up.rs
+++ src/rv_core/instruction/executor/v/vfslide1up.rs
@@ -1,10 +1,24 @@
 use crate::prelude::*;
 
+use crate::rv_core::registers::vector::Vreg;
 use crate::rv_core::{
     instruction::format::Opfvf,
     registers::{FloatRegisters, VectorRegisters},
 };
 
 pub fn vf(Opfvf { vd, rs1, vs2, vm }: Opfvf, v: &mut VectorRegisters, f: &FloatRegisters) {
-    todo!()
+    let first_value = f64::to_le_bytes(f[rs1]);
+    let vreg_values: Vreg = first_value[0..v.vec_engine.sew.byte_length()]
+        .into_iter()
+        .copied()
+        .chain(v.get(vs2).iter_byte().skip(v.vec_engine.sew.byte_length()))
+        .take(v.vec_engine.vlmax())
+        .collect();
+
+    let vreg = vreg_values
+        .iter_eew()
+        .masked_map(v.default_mask(vm), v.get(vd).iter_eew(), |vd| vd)
+        .collect_with_eew(v.vec_engine.sew.clone());
+
+    v.apply(vd, vreg);
 }
