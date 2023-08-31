0a2991d1a0a26447678f3a4d0b43b52a89136360
PawelPerek <pawelperek99@gmail.com>
2023-08-31 23:03:00 +0200

    Implement fp-mask vector instructions

--- src/rv_core/instruction/executor/v/vmfeq.rs
+++ src/rv_core/instruction/executor/v/vmfeq.rs
@@ -1,9 +1,21 @@
 use crate::rv_core::instruction::executor::prelude::*;
 
-pub fn vv(Opfvv { dest, vs1, vs2, vm }: Opfvv, v: &mut VectorRegisters) {
-    todo!()
+pub fn vv(Opfvv { dest: vd, vs1, vs2, vm }: Opfvv, v: &mut VectorRegisters) {
+    let vreg = izip!(v.get(vs2).iter_fp(), v.get(vs1).iter_fp())
+        .masked_map(v.default_mask(vm), v.get(vd).iter_eew(), |(vs2, vs1)| {
+            if vs2 == vs1 { 1 } else { 0 }
+        })
+        .collect_with_eew(v.vec_engine.sew.clone());
+
+    v.apply(vd, vreg);
 }
 
 pub fn vf(Opfvf { vd, rs1, vs2, vm }: Opfvf, v: &mut VectorRegisters, f: &FloatRegisters) {
-    todo!()
+    let vreg = v.get(vs2).iter_fp()
+        .masked_map(v.default_mask(vm), v.get(vd).iter_eew(), |vs2| {
+            if vs2 == ArbitraryFloat::copy_type(&vs2, f[rs1]) { 1 } else { 0 }
+        })
+        .collect_with_eew(v.vec_engine.sew.clone());
+
+    v.apply(vd, vreg);
 }
--- src/rv_core/instruction/executor/v/vmfge.rs
+++ src/rv_core/instruction/executor/v/vmfge.rs
@@ -1,5 +1,11 @@
 use crate::rv_core::instruction::executor::prelude::*;
 
 pub fn vf(Opfvf { vd, rs1, vs2, vm }: Opfvf, v: &mut VectorRegisters, f: &FloatRegisters) {
-    todo!()
+    let vreg = v.get(vs2).iter_fp()
+        .masked_map(v.default_mask(vm), v.get(vd).iter_eew(), |vs2| {
+            if vs2 >= ArbitraryFloat::copy_type(&vs2, f[rs1]) { 1 } else { 0 }
+        })
+        .collect_with_eew(v.vec_engine.sew.clone());
+
+    v.apply(vd, vreg);
 }
