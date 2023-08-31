97c84018ba27b6092ae2b6dc73a2b4cb936071cf
PawelPerek <pawelperek99@gmail.com>
2023-09-01 00:22:53 +0200

    Add saturating add/sub instructions

--- src/rv_core/instruction/executor/v/vsaddu.rs
+++ src/rv_core/instruction/executor/v/vsaddu.rs
@@ -1,13 +1,53 @@
 use crate::rv_core::instruction::executor::prelude::*;
 
 pub fn vv(Opivv { vd, vs1, vs2, vm }: Opivv, v: &mut VectorRegisters) {
-    todo!()
+    let vreg = izip!(v.get(vs2).iter_eew(), v.get(vs1).iter_eew())
+        .masked_map(v.default_mask(vm), v.get(vd).iter_eew(), |(vs2, vs1)| {
+            match v.vec_engine.sew.bit_length() {
+                8 => (vs2 as  u8).saturating_add(vs1 as  u8) as u64,
+                16 => (vs2 as u16).saturating_add(vs1 as u16) as u64,
+                32 => (vs2 as u32).saturating_add(vs1 as u32) as u64,
+                64 => vs2.saturating_add(vs1),
+                _ => unimplemented!()
+            }
+        })
+        .collect_with_eew(v.vec_engine.sew.clone());
+
+    v.apply(vd, vreg);
 }
 
 pub fn vx(Opivx { vd, rs1, vs2, vm }: Opivx, v: &mut VectorRegisters, x: &IntegerRegisters) {
-    todo!()
+    let vreg = v
+        .get(vs2)
+        .iter_eew()
+        .masked_map(v.default_mask(vm), v.get(vd).iter_eew(), |vs2| {
+            match v.vec_engine.sew.bit_length() {
+                8 => (vs2 as u8).saturating_add(x[rs1] as u8) as u64,
+                16 => (vs2 as u16).saturating_add(x[rs1] as u16) as u64,
+                32 => (vs2 as u32).saturating_add(x[rs1] as u32) as u64,
+                64 => vs2.saturating_add(x[rs1]),
+                _ => unimplemented!()
+            }
+        })
+        .collect_with_eew(v.vec_engine.sew.clone());
+
+    v.apply(vd, vreg);
 }
 
 pub fn vi(Opivi { vd, imm5, vs2, vm }: Opivi, v: &mut VectorRegisters) {
-    todo!()
+    let vreg = v
+        .get(vs2)
+        .iter_eew()
+        .masked_map(v.default_mask(vm), v.get(vd).iter_eew(), |vs2| {
+            match v.vec_engine.sew.bit_length() {
+                8 => (vs2 as u8).saturating_add(imm5 as u8) as u64,
+                16 => (vs2 as u16).saturating_add(imm5 as u16) as u64,
+                32 => (vs2 as u32).saturating_add(imm5 as u32) as u64,
+                64 => vs2.saturating_add(imm5 as u64),
+                _ => unimplemented!()
+            }
+        })
+        .collect_with_eew(v.vec_engine.sew.clone());
+
+    v.apply(vd, vreg);
 }
--- src/rv_core/instruction/executor/v/vssub.rs
+++ src/rv_core/instruction/executor/v/vssub.rs
@@ -1,9 +1,35 @@
 use crate::rv_core::instruction::executor::prelude::*;
 
 pub fn vv(Opivv { vd, vs1, vs2, vm }: Opivv, v: &mut VectorRegisters) {
-    todo!()
+    let vreg = izip!(v.get(vs2).iter_eew(), v.get(vs1).iter_eew())
+        .masked_map(v.default_mask(vm), v.get(vd).iter_eew(), |(vs2, vs1)| {
+            match v.vec_engine.sew.bit_length() {
+                8 => (vs2 as i8).saturating_sub(vs1 as i8) as u64,
+                16 => (vs2 as i16).saturating_sub(vs1 as i16) as u64,
+                32 => (vs2 as i32).saturating_sub(vs1 as i32) as u64,
+                64 => (vs2 as i64).saturating_sub(vs1 as i64) as u64,
+                _ => unimplemented!()
+            }
+        })
+        .collect_with_eew(v.vec_engine.sew.clone());
+
+    v.apply(vd, vreg);
 }
 
 pub fn vx(Opivx { vd, rs1, vs2, vm }: Opivx, v: &mut VectorRegisters, x: &IntegerRegisters) {
-    todo!()
+    let vreg = v
+        .get(vs2)
+        .iter_eew()
+        .masked_map(v.default_mask(vm), v.get(vd).iter_eew(), |vs2| {
+            match v.vec_engine.sew.bit_length() {
+                8 => (vs2 as i8).saturating_sub(x[rs1] as i8) as u64,
+                16 => (vs2 as i16).saturating_sub(x[rs1] as i16) as u64,
+                32 => (vs2 as i32).saturating_sub(x[rs1] as i32) as u64,
+                64 => (vs2 as i64).saturating_sub(x[rs1] as i64) as u64,
+                _ => unimplemented!()
+            }
+        })
+        .collect_with_eew(v.vec_engine.sew.clone());
+
+    v.apply(vd, vreg);
 }
