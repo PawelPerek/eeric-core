dc67de1ca3835c980fc0f0c73f5c3bb233c4920f
PawelPerek <pawelperek99@gmail.com>
2023-09-13 23:44:29 +0200

    Implement api for .data support

--- src/rv_core/memory.rs
+++ src/rv_core/memory.rs
@@ -2,21 +2,32 @@ use super::snapshot::Snapshotable;
 
 #[derive(Clone, PartialEq)]
 #[cfg_attr(debug_assertions, derive(Debug))]
-pub struct Memory(Vec<u8>);
+pub struct Memory {
+    raw: Vec<u8>,
+    data_ptr: usize
+}
 
 impl Snapshotable for Memory {
     type Snapshot = Vec<u8>;
 
     fn snapshot(&self) -> Self::Snapshot {
-        self.0.clone()
+        self.raw.clone()
     }
 }
 
 impl Memory {
+    fn new(raw: Vec<u8>) -> Self {
+        let data_ptr = raw.len();
+        Self {
+            raw,
+            data_ptr
+        }
+    }
+
     pub fn get<const BYTES: usize>(&self, address: usize) -> [u8; BYTES] {
         let mut bytes = [0; BYTES];
 
-        bytes[..BYTES].copy_from_slice(&self.0[address..(BYTES + address)]);
+        bytes[..BYTES].copy_from_slice(&self.raw[address..(BYTES + address)]);
 
         bytes
     }
@@ -25,7 +36,7 @@ impl Memory {
         let mut bytes = [0; BYTES];
 
         for (offset, byte_element) in bytes.iter_mut().enumerate().take(BYTES) {
-            let Some(byte) = self.0.get(address + offset).cloned() else {
+            let Some(byte) = self.raw.get(address + offset).cloned() else {
                 return None;
             };
 
@@ -36,13 +47,18 @@ impl Memory {
     }
 
     pub fn set<const BYTES: usize>(&mut self, address: usize, value: [u8; BYTES]) {
-        self.0[address..(BYTES + address)].copy_from_slice(&value[..BYTES]);
+        self.raw[address..(BYTES + address)].copy_from_slice(&value[..BYTES]);
+    }
+
+    pub fn assign(&mut self, data: &[u8]) {
+        &self.raw[self.data_ptr - data.len() .. self.data_ptr].copy_from_slice(data);
+        self.data_ptr -= data.len();
     }
 }
 
 impl From<Vec<u8>> for Memory {
     fn from(bytes: Vec<u8>) -> Self {
-        Self(
+        Self::new(
             bytes
                 .iter()
                 .cloned()
@@ -54,6 +70,29 @@ impl From<Vec<u8>> for Memory {
 
 impl Default for Memory {
     fn default() -> Self {
-        Self(vec![0; 0x1000])
+        Self::new(vec![0; 0x1000])
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn assign_works() {
+        let mut mem = Memory::new(vec![0; 0x10]);
+
+        assert_eq!(mem.data_ptr, 0x10);
+
+        mem.assign(&[1,2]);
+
+        assert_eq!(mem.data_ptr, 0xE);
+        assert_eq!(mem.raw, &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2]);
+
+        mem.assign(&[5,10,15]);
+
+        assert_eq!(mem.data_ptr, 0xB);
+        assert_eq!(mem.raw, &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 10, 15, 1, 2]);
     }
 }
